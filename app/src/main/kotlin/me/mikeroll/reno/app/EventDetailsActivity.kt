d59e5a96f52f23cb62e8662aa6a67f19b714f87a
Mikhail Bulash <scaryspiderpig@gmail.com>
2015-05-14 04:03:52 +0300

    Load event data in details activity

--- app/src/main/kotlin/me/mikeroll/reno/app/EventDetailsActivity.kt
+++ app/src/main/kotlin/me/mikeroll/reno/app/EventDetailsActivity.kt
@@ -1,7 +1,32 @@
 package me.mikeroll.reno.app
 
 import android.app.Activity
+import android.os.Bundle
+import com.google.gson.Gson
+import me.mikeroll.reno.client.Event
+import org.jetbrains.anko.*
+import kotlin.properties.Delegates
 
 public class EventDetailsActivity : Activity() {
 
+    private val event: Event? by Delegates.lazy {
+        val data = intent.getStringExtra("event")
+        if (data != null) {
+            Gson().fromJson(data, javaClass<Event>())
+        } else {
+            null
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (event != null) {
+            linearLayout {
+                button(event!!.artists[0].name)
+            }
+        } else {
+            longToast("Failed loading event :(")
+            finish()
+        }
+    }
 }
